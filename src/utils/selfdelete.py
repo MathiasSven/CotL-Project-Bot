13c04bd8d8fb24cbacb4ca0504a18342687120c1
Mathias Sven <mathiassven2@hotmail.com>
2021-02-27 23:10:08 -0300

    fixed selfdelete.py

--- src/utils/selfdelete.py
+++ src/utils/selfdelete.py
@@ -1,11 +1,12 @@
 import asyncio
 
 from discord.ext.commands import CommandInvokeError
+from discord.errors import Forbidden
 
 
 async def self_delete(ctx, time=0.5):
     await asyncio.sleep(time)
     try:
         await ctx.message.delete()
-    except CommandInvokeError:
+    except (CommandInvokeError, Forbidden):
         pass
