9e92c7ebfb1b6aa11899b19a962ecd1f5a634843
Mathias Sven <mathiassven2@hotmail.com>
2021-07-25 08:33:29 -0300

    Fixed DM system problem

--- src/cogs/tickets.py
+++ src/cogs/tickets.py
@@ -26,15 +26,24 @@ class Tickets(commands.Cog):
                                           f", and wait until one of our interviewers adds you to create a group chat.")
         embed.set_footer(text="Cataclysm", icon_url=self.GUILD.icon_url)
 
-        message = await self.bot.APPLICATION_CHANNEL.send(embed=embed)
+        message = await self.APPLICATION_CHANNEL.send(embed=embed)
         await message.add_reaction(EMOJI[':clipboard:'])
         return message
 
     async def create_interviewer_embed(self, applicant: discord.Member) -> discord.Message:
         embed = discord.Embed(title="New Applicant", colour=discord.Colour(self.bot.COLOUR),
-                              description=f"User: {applicant.mention}\n"
-                                          f"The first on the trigger, react with {EMOJI[':white_check_mark:']}")
-        embed.set_footer(text="Unassigned")
+                              description=f"The first on the trigger, react with {EMOJI[':white_check_mark:']}")
+        embed.set_author(name=applicant.display_name, icon_url=applicant.avatar_url)
+
+        embed.add_field(name=f"Applicant:",
+                        value=applicant.mention,
+                        inline=True)
+
+        embed.add_field(name=f"Assigned To:",
+                        value=f"Unassigned",
+                        inline=True)
+
+        embed.set_footer(text="Last Event")
         embed.timestamp = datetime.datetime.utcnow()
 
         message = await self.INTERVIEWER_CHANNEL.send(self.INTERVIEWER_ROLE.mention, embed=embed)
@@ -47,8 +56,9 @@ class Tickets(commands.Cog):
         self.GUILD = self.bot.get_guild(self.bot.GUILD_ID)
         self.INTERVIEWER_CHANNEL = self.GUILD.get_channel(self.INTERVIEWER_CHANNEL_ID)
         self.INTERVIEWER_ROLE = self.GUILD.get_role(self.INTERVIEWER_ROLE_ID)
+        self.APPLICATION_CHANNEL = self.GUILD.get_channel(self.bot.APPLICATION_CHANNEL_ID)
 
-        messages_w_embeds = [message async for message in self.bot.APPLICATION_CHANNEL.history(limit=100) if bool(message.embeds)]
+        messages_w_embeds = [message async for message in self.APPLICATION_CHANNEL.history(limit=100) if bool(message.embeds)]
         try:
             application_message = next(filter(lambda message: message.embeds[0].title == self.applications_embed_title, messages_w_embeds))
         except StopIteration:
@@ -63,7 +73,7 @@ class Tickets(commands.Cog):
             if str(payload.emoji) == EMOJI[':clipboard:']:
                 await self.create_interviewer_embed(payload.member)
 
-            message = await self.bot.APPLICATION_CHANNEL.fetch_message(payload.message_id)
+            message = await self.APPLICATION_CHANNEL.fetch_message(payload.message_id)
             await message.remove_reaction(payload.emoji, payload.member)
 
     @commands.Cog.listener()
@@ -85,13 +95,20 @@ class Tickets(commands.Cog):
             interviewer_embed = message.embeds.pop()
         except IndexError:
             return
-        if "New Applicant" not in interviewer_embed.title:
+        if "New Applicant" not in interviewer_embed.title or not any((reaction.me for reaction in message.reactions)):
+            await message.remove_reaction(payload.emoji, payload.member)
             return
 
-        await message.clear_reactions()
-        interviewer_embed.timestamp = datetime.datetime.utcnow()
-        interviewer_embed.set_footer(text=f"Assigned to {member.display_name}")
-        await message.edit(embed=interviewer_embed)
+        print(message.reactions)
+
+        if str(payload.emoji) == EMOJI[':white_check_mark:']:
+            await message.clear_reactions()
+            interviewer_embed.timestamp = datetime.datetime.utcnow()
+            interviewer_embed.description = ""
+            interviewer_embed.set_field_at(index=1, name=f"Assigned To:", value=member.mention)
+            await message.edit(embed=interviewer_embed)
+        else:
+            await message.remove_reaction(payload.emoji, payload.member)
 
     @commands.Cog.listener()
     async def on_ready(self):
