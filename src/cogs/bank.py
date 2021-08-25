d1b276c2afedf81ecf84ebb1d13bfe872381fa25
Mathias Sven <mathiassven2@hotmail.com>
2021-08-25 17:03:26 +0100

    joke cass

--- src/cogs/bank.py
+++ src/cogs/bank.py
@@ -874,7 +874,6 @@ class Bank(commands.Cog):
             await message.author.send("https://www.sanfransentinel.com/check-your-holdings.html")
             return
 
-        print(message.content)
         if re.findall(pattern=r"(canada)|(.anada)|(c.nada)|(ca.ada)|(can.da)|(cana.a)|(canad.)|(mounties)|(maple)|(🇨🇦)", string=message.content, flags=re.MULTILINE | re.IGNORECASE):
             if message.guild:
                 await asyncio.sleep(0.5)
